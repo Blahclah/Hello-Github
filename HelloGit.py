6055673ff33ede8cd54faf95e6c2e4e76a61013a
Adrian Swindle <adrianswindle@Adrians-MacBook-Air.local>
2020-02-03 08:40:29 -0600

    First python

---
+++ HelloGit.py
@@ -0,0 +1,19 @@
+# comment
+print("Hello, World!")
+a =6
+b= 5
+print(a+b)
+
+
+# hot to make a list
+myList = [1,2,3]
+
+# how to acces elements of a list
+print(myList[1])
+
+#how to write a function
+def sumFunction(a,b):
+	return a+b
+
+#how to call a function
+print(sumFunction(2,20))
